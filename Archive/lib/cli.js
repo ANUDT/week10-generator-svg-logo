fce1332c7c134acba63469668427cb168ef375ed
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-03-28 21:46:55 +0000

    aMENDED

---
+++ Archive/lib/cli.js
@@ -0,0 +1,38 @@
+const inquirer = require("inquirer");
+const { writeFile } = require("fs/promises");
+const { Circle, Square } = require("./shapes");
+const questions = require("./questions");
+const { LogoText, Svg } = require("./svg");
+
+class CLI {
+  constructor() {}
+  run() {
+    return inquirer
+      .prompt(questions)
+      .then((responses) => {
+        const svg = this.generateSvg(responses);
+        const markup = svg.render();
+        return writeFile("logo.svg", markup);
+      })
+      .then(() => console.log("logo created"))
+      .catch((err) => {
+        console.log(err);
+        console.log("error, please double check");
+      });
+  }
+
+  generateSvg(options) {
+    const text = LogoText(options.text, options.textColour);
+
+    switch (options.shape) {
+      case "square":
+        const square = new Square(options.shapeColour);
+        return new Svg(square, text);
+      case "circle":
+        const cirlce = new Circle(options.shapeColour);
+        return new Svg(cirlce, text);
+    }
+  }
+}
+
+module.exports = CLI;
---
+++ Archive/lib/questions.js
@@ -0,0 +1,25 @@
+const questions = [
+  {
+    name: "text",
+    type: "input",
+    message: "Enter text for the logo. (Must not be more than 3 characters.)",
+  },
+  {
+    name: "textColor",
+    type: "input",
+    message: "Enter a text color",
+  },
+  {
+    name: "shape",
+    type: "list",
+    message: "Select a shape for the logo",
+    choices: ["circle", "square", "triangle"],
+  },
+  {
+    name: "shapeColor",
+    type: "input",
+    message: "Enter a shape color",
+  },
+];
+
+module.exports = questions;
